43ad5c71c4bae2fd3d196c99ec5e292d0d45db25
Gavrilov Basil <gavrilov.vasia@gmail.com>
2017-06-27 17:16:59 +0300

    Some base structure calsses

---
+++ app/src/main/java/gav/vaska/basil/hellokitty/ConversationActivity.java
@@ -0,0 +1,22 @@
+package gav.vaska.basil.hellokitty;
+
+/**
+ * Created by basil on 27.06.17.
+ */
+
+public class ConversationActivity implements Callback {
+    public void shakingStarted()
+    {
+        //todo: display clouds start
+    }
+
+    public void shakingStopped()
+    {
+        //todo: display clouds finish
+    }
+
+    public void viewAnswer()
+    {
+
+    }
+}
---
+++ app/src/main/java/gav/vaska/basil/hellokitty/RandomImage.java
@@ -0,0 +1,9 @@
+package gav.vaska.basil.hellokitty;
+
+/**
+ * Created by basil on 27.06.17.
+ */
+
+public class RandomImage {
+
+}
