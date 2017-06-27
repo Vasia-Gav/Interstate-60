5ff8387d1e3783934b29bae8409f48cc0df28a45
Gavrilov Basil <gavrilov.vasia@gmail.com>
2017-06-27 16:59:46 +0300

    Shaker class. Shake to get the answer functionality part.

---
+++ app/src/main/java/gav/vaska/basil/hellokitty/Callback.java
@@ -0,0 +1,10 @@
+package gav.vaska.basil.hellokitty;
+
+/**
+ * Created by basil on 27.06.17.
+ */
+
+public interface Callback {
+    void shakingStarted();
+    void shakingStopped();
+}
---
+++ app/src/main/java/gav/vaska/basil/hellokitty/MainActivity.java
@@ -0,0 +1,13 @@
+package gav.vaska.basil.hellokitty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
